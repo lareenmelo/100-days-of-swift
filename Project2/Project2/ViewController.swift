38757d37401e69214df4dc7b162fde96c25aeef0
Lareen Melo <lareenmelo@gmail.com>
2019-09-09 20:07:59 -0400

    Add project #2 - from outlets to actions: creating an IBAction

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
         askQuestion()
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
 
         button1.setImage(UIImage(named: countries[0]), for: .normal)
@@ -43,9 +43,23 @@ class ViewController: UIViewController {
         correctAnswer = Int.random(in: 0...2)
         title = countries[correctAnswer].uppercased()
 
-
     }
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
+    
 
 }
 
