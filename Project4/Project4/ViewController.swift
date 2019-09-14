6c7e4ad492da56dafb8b5f4be6d8ce4aa5175b31
Lareen Melo <lareenmelo@gmail.com>
2019-09-14 14:11:20 -0400

    Add project #4 - web view

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
 
+    // gets called before viewDidLoad
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+
     }
 
 
