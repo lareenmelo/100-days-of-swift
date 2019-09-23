19810e41f4a2d99fb2ca6dc5a43878f9d92eae8b
Lareen Melo <lareenmelo@gmail.com>
2019-09-23 15:36:04 -0400

    Add Day 32 with bonus point

---
+++ Milestone2/Milestone2/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Milestone2
+//
+//  Created by Lareen Melo on 9/23/19.
+//  Copyright © 2019 Lareen Melo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var shoppingList = [String]()
+//    let item = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Clear", style: .done, target: self, action: #selector(clearList))
+        
+        title = "✨Your Magical List✨"
+    }
+    
+    func submit(_ answer: String) {
+        shoppingList.append(answer)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+    @objc func addItem() {
+        let ac = UIAlertController(title: "Enter item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let add = UIAlertAction(title: "Add", style: .default) { [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(add)
+        present(ac, animated: true)
+    }
+    
+    
+    @objc func clearList() {
+        shoppingList.removeAll()
+        tableView.reloadData()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+}
+
