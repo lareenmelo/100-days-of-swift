d6ed551dd79c13a3e91c4e76a2d8a52b89bfa73a
Lareen Melo <lareenmelo@gmail.com>
2019-09-23 16:12:31 -0400

    Add Day 33

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Lareen Melo on 9/23/19.
+//  Copyright © 2019 Lareen Melo. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Lareen Melo on 9/23/19.
+//  Copyright © 2019 Lareen Melo. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
