5e82207021a4ccf8c15253c1f10e9253b51ed8ee
Lareen Melo <lareenmelo@gmail.com>
2019-09-25 15:15:19 -0400

    Add Day 35

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UITableViewController {
         
         let urlString: String
 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
         if navigationController?.tabBarItem.tag == 0 {
             // urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
@@ -35,6 +36,12 @@ class ViewController: UITableViewController {
         showError()
     }
 
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Credits", message: "Credits goes to the We The People API of the Whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
     func showError() {
         let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
