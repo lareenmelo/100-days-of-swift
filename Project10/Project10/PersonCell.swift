00a7b5eac09abcb9113a22ee2009c95fe8ab6539
Lareen Melo <lareenmelo@gmail.com>
2019-10-07 16:31:07 -0400

    Add Day 42

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Lareen Melo on 10/7/19.
+//  Copyright © 2019 Lareen Melo. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Lareen Melo on 10/7/19.
+//  Copyright © 2019 Lareen Melo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            // we failed to get a PersonCell – bail out!
+            fatalError("Unable to dequeue PersonCell.")
+        }
+
+        // if we're still here it means we got a PersonCell, so we can return it
+        return cell
+    }
+
+
+}
+
