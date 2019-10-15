eb49728f0c65128f1e27d31b1362439f92a2bc7e
Lareen Melo <lareenmelo@gmail.com>
2019-10-15 16:40:19 -0400

    Add Day 49 - Finish Project 12b

---
+++ Project12b/Project10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Lareen Melo on 10/8/19.
+//  Copyright © 2019 Lareen Melo. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject, Codable {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}
