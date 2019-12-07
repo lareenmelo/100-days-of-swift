57da9fa92e7c1068b16f3664f3450d842f9eb11a
Lareen Melo <lareenmelo@gmail.com>
2019-12-07 14:20:55 -0500

    Add Day 74 Challenge #1

---
+++ Milestone4/Milestone4/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  Milestone4
+//
+//  Created by Lareen Melo on 12/7/19.
+//  Copyright © 2019 Lareen Melo. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    var content = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Milestone4/Milestone4/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Milestone4
+//
+//  Created by Lareen Melo on 12/7/19.
+//  Copyright © 2019 Lareen Melo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    let notes = [(id: "Note#1", content: "WHERE"), (id: "Note#2", content: "MY"), (id: "Note#3", content: "PPL"), (id: "Note#4", content: "AT")]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "note", for: indexPath)
+        cell.textLabel?.text = notes[indexPath.row].id
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+
+}
+
