90faa0759394817f36944ee288e737fabf06af07
Lareen Melo <lareenmelo@gmail.com>
2019-12-12 15:05:54 -0500

    Add progress

--- Milestone4/Milestone4/ViewController.swift
+++ Milestone4/Milestone4/ViewController.swift
@@ -12,17 +12,59 @@ class ViewController: UITableViewController {
     
     var notes = [Note]()
     let userDefaults = UserDefaults.standard
+    
+    // MARK: Navigation Items Declaration
+    @IBOutlet var editButton: UIBarButtonItem!
+    
+    // MARK: Toolbar Items Declaration
+    var composeButton: UIBarButtonItem!
+    var totalNotes: UIBarButtonItem!
+    var deleteNotesButton: UIBarButtonItem!
+    // Empty button to replicate notes layout
+    var space: UIBarButtonItem!
 
+    // Toggles editButton title so it goes along with the correct actions.
+    var editScene: Bool = false {
+        didSet {
+            if editScene {
+                editButton.title = "Cancel"
+            } else {
+                editButton.title = "Edit"
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(createNote))
+        editButton.title = "Edit"
+        
+        // MARK: Toolbar Items Inits
+        composeButton = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(createNote))
+        space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        
+        // FIXME: hacer el force de una variable que cambie con el didSet, kkc.
+        deleteNotesButton = UIBarButtonItem(title: "Delete All", style: .plain, target: self, action: #selector(deleteNotes))
+
+        // FIXME: notes aren't being updated.
+        let totalNotes = UIBarButtonItem(title: "\(notes.count) Notes", style: .plain, target: self, action: nil)
+        
+        // TODO: init two arrays here.
+        
+        toolbarItems = [space, totalNotes, space, composeButton]
+        navigationItem.rightBarButtonItem = editButton
+        navigationController?.isToolbarHidden = false
     }
     
     override func viewWillAppear(_ animated: Bool) {
         notes = Defaults.load()
         tableView.reloadData()
 
+    }
+    
+    @IBAction func editingScene(_ sender: Any) {
+        editScene.toggle()
+
+        toolbarItems = [space, deleteNotesButton]
     }
     
     @objc func createNote() {
@@ -41,7 +83,10 @@ class ViewController: UITableViewController {
             }
         }
     }
-
+    
+    @objc func deleteNotes() {
+        print("will do the magic to delete the notes")
+    }
     
     // MARK: Table View Controller Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -61,7 +106,6 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // TODO: remove from user defaults
             notes.remove(at: indexPath.row)
 
             DispatchQueue.global().async { [weak self] in
