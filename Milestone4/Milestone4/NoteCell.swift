0b68983d7b8ceed7e81613d53349f0065fea57b8
Lareen Melo <lareenmelo@gmail.com>
2019-12-14 12:01:12 -0500

    Add date label

--- Milestone4/Milestone4/NoteCell.swift
+++ Milestone4/Milestone4/NoteCell.swift
@@ -11,18 +11,5 @@ import UIKit
 class NoteCell: UITableViewCell {
     @IBOutlet var noteTitle: UILabel!
     @IBOutlet var noteDescription: UILabel!
-    
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        // Initialization code
-//    }
-
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//        
-//        
-//
-//        // Configure the view for the selected state
-//    }
-
+    @IBOutlet var creationDate: UILabel!
 }
