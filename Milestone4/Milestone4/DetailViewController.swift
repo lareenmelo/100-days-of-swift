abd1f61537920730677e1b8ce041a55e7834cc6c
Lareen Melo <lareenmelo@gmail.com>
2019-12-15 11:59:53 -0500

    Add Keyboard interaction works

--- Milestone4/Milestone4/DetailViewController.swift
+++ Milestone4/Milestone4/DetailViewController.swift
@@ -41,6 +41,10 @@ class DetailViewController: UIViewController, UITextViewDelegate {
         originalText = selectedNote.content
         noteTextView.text = selectedNote.content
         
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -121,4 +125,24 @@ class DetailViewController: UIViewController, UITextViewDelegate {
         ac.popoverPresentationController?.barButtonItem = shareButton
         present(ac, animated: true)
     }
+    
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            noteTextView.contentInset = .zero
+        } else {
+            noteTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        
+        noteTextView.scrollIndicatorInsets = noteTextView.contentInset
+        
+        let selectedRange = noteTextView.selectedRange
+        noteTextView.scrollRangeToVisible(selectedRange)
+    }
+    
 }
