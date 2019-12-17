c957610fb2c26c2b33f3a7bd22b111bd8880b746
Lareen Melo <lareenmelo@gmail.com>
2019-12-17 14:59:25 -0500

    Add Project #22 - Setting up

---
+++ Project22/Project22/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by Lareen Melo on 12/17/19.
+//  Copyright © 2019 Lareen Melo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
