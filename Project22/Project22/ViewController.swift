d8b04ba20424f44364d0f1a34a1db0363a5e75aa
Lareen Melo <lareenmelo@gmail.com>
2019-12-17 15:21:10 -0500

    Add Project #22 - requesting location (core location)

--- Project22/Project22/ViewController.swift
+++ Project22/Project22/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet var distanceReading: UILabel!
+    // lets us configure how we want to be notified about location and will deliver
+    // location updates
+    var locationManager: CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .gray
     }
 
 
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    print("do things")
+                }
+            }
+        }
+    }
 }
 
