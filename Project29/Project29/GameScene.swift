c3cd69a5917414428391e9a0bc51c704e2152d6c
Lareen Melo <lareenmelo@gmail.com>
2020-01-18 21:35:03 -0500

    Add Day 94

---
+++ Project29/Project29/GameScene.swift
@@ -0,0 +1,47 @@
+//
+//  GameScene.swift
+//  Project29
+//
+//  Created by Lareen Melo on 1/18/20.
+//  Copyright © 2020 Lareen Melo. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+enum CollisionTypes: UInt32 {
+    case banana = 1
+    case building = 2
+    case player = 3
+}
+
+class GameScene: SKScene {
+    weak var viewController: GameViewController!
+    var buildings = [BuildingNode]()
+
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
+        
+        createBuildings()
+    }
+    
+    func createBuildings() {
+        var currentX: CGFloat = -15
+        
+        while currentX < 1024 {
+            let size = CGSize(width: Int.random(in: 2...4) * 40, height: Int.random(in: 300...600))
+            currentX += size.width + 2
+            
+            let building = BuildingNode(color: UIColor.red, size: size)
+            building.position = CGPoint(x: currentX - (size.width / 2), y: size.height / 2)
+            building.setup()
+            addChild(building)
+            
+            buildings.append(building)
+            
+        }
+    }
+    
+    
+}
